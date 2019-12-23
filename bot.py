8206d1ab78ce577c03bd9e1c3c003920948a5edb
Willy <willychan@live.ca>
2019-12-23 04:14:18 -0500

    Subclass bot

--- bot.py
+++ bot.py
@@ -1,22 +1,33 @@
 import twitchio
+from twitchio.dataclasses import Context
 from twitchio.ext import commands
+
+import platform
+from datetime import datetime
 
 from config import IRC_TOKEN, NICK, INIT_CHANNELS
 
-bot = commands.Bot(
-    irc_token=IRC_TOKEN,
-    prefix='!',
-    nick=NICK,
-    initial_channels=INIT_CHANNELS
-)
 
-@bot.event
-async def event_ready():
-    print(f'Ready | {bot.nick}')
+class CenticeBot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            irc_token=IRC_TOKEN,
+            nick=NICK,
+            prefix='!',
+            initial_channels=INIT_CHANNELS
+        )
 
-@bot.command(name='test')
-async def test(ctx: twitchio.dataclasses.Context):
-    await ctx.send('test success')
+    async def event_ready(self):
+        print(f'\nLogged in as: {self.nick}\n\n'
+              f'Python Version: {platform.python_version()}\n'
+              f'Library Version: {twitchio.__version__}')
+
+        print(f'Ready! {datetime.utcnow()}\n')
+
+    @commands.command(name='test')
+    async def test(self, ctx: Context):
+        await ctx.send('test success')
 
 
+bot = CenticeBot()
 bot.run()
